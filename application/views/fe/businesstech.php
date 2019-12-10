7e530fd3386448a728fe53c513d82068015856a5
Nahashon <nahashonmbuci@gmail.com>
2019-12-10 20:35:30 +0300

    Business Technology

--- application/views/fe/businesstech.php
+++ application/views/fe/businesstech.php
@@ -105,20 +105,73 @@
 <!-- About our business solution  -->
     <div class="managment-area-2 content-area-8">
         <div class="container">
+        <!-- Heading -->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="main-title">
+                    <h1><span>About </span>our Solutions </h1>
+                </div>
+            </div>
+        </div>
+<!-- Accordion -->
             <div class="row">
                 <div class="col-md-6">
-                    <div class="managment-info">
-                        <h1><span>About </span> Our Business <br> Technologies</h1>
-                        <ul class="">
-                            <li><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Transparent </li>
-                            <li><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Transparent </li>
-                            <li><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Transparent </li>
-                            <li><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Transparent </li>
-                            <li><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Transparent </li>
-                        </ul>
-                    </div>
-                </div>
+                    <div class="tabbing tabbing-box">
+                        <div id="faq" class="faq-accordion">
+                            <div class="card m-b-0">
+							
+							<!-- Excellence -->
+                            <div class="card-header">
+                                <a class="card-title collapsed" data-toggle="collapse" data-parent="#faq" href="#collapse1">
+                                    <span class="answer-helpful"><i class="fa fa-thumbs-o-up title"></i></span> Excellence
+                                </a>
+                            </div>
+                            <div id="collapse1" class="card-block collapse">
+                               <p class="text-justify">We strive for continuous improvement. We aim at producing the best we are capable of as we challenge the process and opinions as we endlessly learn and grow to unleash potentials and stretch our limits</p>
+                                <hr>
+                            </div>
 
+                            <!-- Quality -->
+                            <div class="card-header">
+                                <a class="card-title collapsed" data-toggle="collapse" data-parent="#faq" href="#collapse2">
+                                    <span class="answer-helpful"><i class="fa fa-thumbs-o-up title"></i></span> Quality
+                                </a>
+                            </div>
+                            <div id="collapse2" class="card-block collapse">
+                                <p class="text-justify">We take pride in providing high value products and services that we stand behind to ensures customer satisfaction, profitability to meet or exceed their expectations.</p>
+                                <hr>
+                            </div>
+
+                            <!-- Innovation -->
+                            <div class="card-header">
+                                <a class="card-title collapsed" data-toggle="collapse" data-parent="#faq" href="#collapse3">
+                                    <span class="answer-helpful"><i class="fa fa-thumbs-o-up title"></i></span> Innovation
+                                </a>
+                            </div>
+                            <div id="collapse3" class="card-block collapse">
+                                <p>We believe that only innovation can keep us ahead our competitors and as a team we always provides extra emphasis on innovation to serve our customer better and bring value to their business. </p>
+                                <hr>
+                            </div>
+
+                            <!-- Reliability -->
+                            <div class="card-header border-0">
+                                <a class="card-title collapsed" data-toggle="collapse" data-parent="#faq" href="#collapse4">
+                                    <span class="answer-helpful"><i class="fa fa-thumbs-o-up title"></i></span> Reliability
+                                </a>
+                            </div>
+                            <div id="collapse4" class="card-block collapse">
+                                <p>We strive to be there through thick and thin, rain or shine, delivering on every promise, every time. We deliver the promise.</p>
+                                <hr>
+                            </div>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+				</div>
+                
+                <!-- Images -->
                 <div class="col-md-6">
                     <img src="<?php echo base_url(); ?>assets/img/banner-1.jpg"
                     class="img-fluid">
